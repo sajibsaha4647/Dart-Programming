715ea586691521c7a3b773d44966bf34d712c0e5
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-17 17:52:30 +0600

    code tricky

--- tricky.js
+++ tricky.js
@@ -86,23 +86,76 @@
 
 // isPalindrome(121);
 
-var longestCommonPrefix = function (strs) {
-  let str = strs.sort();
-  console.log(str);
-  let first = str[0];
-  let last = str[str.length - 1];
+// var longestCommonPrefix = function (strs) {
+//   let str = strs.sort();
+//   console.log(str);
+//   let first = str[0];
+//   let last = str[str.length - 1];
 
-  let result = "";
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] === last[i]) {
-      result += first[i];
-    } else {
-      break;
-    }
-  }
+//   let result = "";
+//   for (let i = 0; i < first.length; i++) {
+//     if (first[i] === last[i]) {
+//       result += first[i];
+//     } else {
+//       break;
+//     }
+//   }
 
-  return result;
+//   return result;
+// };
+
+// let strs = ["flow", "ddlow", "xlight", "tlows"];
+// longestCommonPrefix(strs);
+
+// var searchInsert = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] >= target) {
+//       return i;
+//     }
+//   }
+//   return nums.length;
+// };
+
+// nums = [1, 3, 5, 6];
+// console.log(searchInsert(nums, 1));
+
+// var lengthOfLastWord = function (s) {
+//   let result = s.split(" ");
+//   let len = result.length;
+//   let arr = [];
+//   for (let i = 0; i < len; i++) {
+//     if (result[i] !== "") {
+//       arr.push(result[i]);
+//     }
+//   }
+//   return arr[arr.length - 1].length;
+// };
+// let s = "q ";
+// console.log(lengthOfLastWord(s));
+
+// var plusOne = function (digits) {
+//   var result = "";
+//   let len = digits.length;
+//   for (let i = 0; i < len; i++) {
+//     result += digits[i];
+//   }
+
+//   let lastone = result[result.length - 1];
+//   let mainitem = parseInt(lastone) + 1;
+//   let slices = result.slice(0, -1);
+//   let sumone = slices.concat(mainitem);
+
+//   let res = [...sumone.toString()];
+//   return res;
+// };
+// let digits = [9, 9];
+// // let digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+// console.log(plusOne(digits));
+
+var plusOne = function (digits) {
+  let sum = BigInt(digits.join("")) + 1n;
+  return sum.toString().split("");
 };
-
-let strs = ["flow", "ddlow", "xlight", "tlows"];
-longestCommonPrefix(strs);
+// let digits = [9, 9];
+let digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+console.log(plusOne(digits));
