4550f221b9ec44fd01c394e0f25a52bb89cc4a03
sajibsaha4647 <sksaha4648@gmail.com>
2022-11-22 17:11:15 +0600

    finally done both

--- tricky.js
+++ tricky.js
@@ -507,23 +507,23 @@ function ListNode(val, next) {
 
 // fib(n);
 
-var addStrings = function (num1, num2) {
-  let i = num1.length - 1;
-  let j = num2.length - 1;
-  let carry = 0;
-  let sum = "";
+// var addStrings = function (num1, num2) {
+//   let i = num1.length - 1;
+//   let j = num2.length - 1;
+//   let carry = 0;
+//   let sum = "";
 
-  for (; i >= 0 || j >= 0 || carry > 0; i--, j--) {
-    const digit1 = i < 0 ? 0 : num1.charAt(i) - "0";
-    const digit2 = j < 0 ? 0 : num2.charAt(j) - "0";
-    const digitsSum = digit1 + digit2 + carry;
-    sum = `${digitsSum % 10}${sum}`;
-    carry = Math.floor(digitsSum / 10);
-  }
-  return sum;
-};
+//   for (; i >= 0 || j >= 0 || carry > 0; i--, j--) {
+//     const digit1 = i < 0 ? 0 : num1.charAt(i) - "0";
+//     const digit2 = j < 0 ? 0 : num2.charAt(j) - "0";
+//     const digitsSum = digit1 + digit2 + carry;
+//     sum = `${digitsSum % 10}${sum}`;
+//     carry = Math.floor(digitsSum / 10);
+//   }
+//   return sum;
+// };
 
-let num1 = "19",
-  num2 = "1239";
+// let num1 = "19",
+//   num2 = "1239";
 
-addStrings(num1, num2);
+// addStrings(num1, num2);
